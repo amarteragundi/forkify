bf1a1eb767a2b3c29d76f40ffdac4ca7b9e3f032
Amar Teragundi <amarteragundi@gmail.com>
2019-04-17 13:57:16 +0530

    display the search results';

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,2 +1,25 @@
 import { elements } from './base';
+
 export const getInput = () => elements.searchInput.value;
+
+const renderRecipe = recipe => {
+console.log(recipe);
+const markUp = `<li>
+    <a class="likes__link" href="#${recipe.recipe_id}">
+        <figure class="likes__fig">
+            <img src="${recipe.image_url}" alt="Test">
+        </figure>
+        <div class="likes__data">
+            <h4 class="likes__name">${recipe.title}</h4>
+            <p class="likes__author">${recipe.publisher}</p>
+        </div>
+    </a>
+</li>`
+
+elements.searchResultList.insertAdjacentHTML('beforeend', markUp);
+}
+
+export const renderResults = recipes => {
+	console.log(recipes);
+	recipes.forEach(renderRecipe)
+}
