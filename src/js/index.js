c43b5fed5e2c6f5789710deeeb4b34c6a5175177
Amar Teragundi <amarteragundi@gmail.com>
2019-04-17 11:37:25 +0530

    adding local state management

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import Search from './models/Search'
 
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
 const search = new Search("pizza");
 search.getResults()
