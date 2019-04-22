646687154f47114d488f2ccee2f7b7784af2ddb0
Amar Teragundi <amarteragundi@gmail.com>
2019-04-22 15:15:40 +0530

    cosuming api key from config file

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
+import {apiConfig} from './config'
 export default class Search {
 		constructor(query){
 			this.query = query;
 		}
 
 	async getResults(){
-		const key = "77d6469266d614a3f0b728ef261a11ed"
 		try{
-			const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+			const res = await axios(`https://www.food2fork.com/api/search?key=${apiConfig.food2fork}&q=${this.query}`);
 			this.result = res.data.recipes;
 			// console.log(this.result)
 		} catch(error){
