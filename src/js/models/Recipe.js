46026e32359923ddf2d7704924af6d2d23ac3ae3
Amar Teragundi <amarteragundi@gmail.com>
2019-04-24 11:11:19 +0530

    chaging the api and grabbing data

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -8,9 +8,12 @@ export default class Recipe {
 
     async getRecipe(){
         try{
-            const res = await axios(`https://www.food2fork.com/api/search?key=${apiConfig.food2fork}&rId=${this.id}`);
-            // /this.result = res.data.recipes;
-			console.log(res)
+            const res = await axios(`https://www.food2fork.com/api/get?key=${apiConfig.food2fork}&rId=${this.id}`);
+            console.log(res)
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.ingredients = res.data.recipe.ingredients;
 		} catch(error){
 			alert(error)
 		}
