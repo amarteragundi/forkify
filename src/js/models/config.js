bb94cde091da142ed3cd04bf7091d16a39d1bfd3
Amar Teragundi <amarteragundi@gmail.com>
2019-04-25 13:47:16 +0530

    backup api in comments

--- src/js/models/config.js
+++ src/js/models/config.js
@@ -1,3 +1,6 @@
 export const apiConfig = {
-    food2fork : '77d6469266d614a3f0b728ef261a11ed'
+    food2fork : '5a5b5def485a8e8bbbbbbb547aecee68'
 }
+
+// 5a5b5def485a8e8bbbbbbb547aecee68 
+// 77d6469266d614a3f0b728ef261a11ed
