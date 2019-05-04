1056a1a49df43e5fb89e19b4a20df0d630554ecb
Amar Teragundi <amarteragundi@gmail.com>
2019-05-04 13:37:27 +0530

    corrections

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -7,7 +7,7 @@ export const renderItem = item => {
                 <input type="number" value=${item.count} step=${item.count} class="shopping__count-value">
                 <p>${item.unit}</p>
             </div>
-            <p class="shopping__description">Pasta</p>
+            <p class="shopping__description">${item.ingredient}</p>
             <button class="shopping__delete btn-tiny">
             <svg>
                 <use href="img/icons.svg#icon-circle-with-cross"></use>
@@ -20,5 +20,5 @@ export const renderItem = item => {
 
 export const deleteItem = id => {
     const item = document.querySelector(`[data-itemid="${id}"]`);
-    item.parentElement.removeChild(item);
+    if(item) item.parentElement.removeChild(item);
 }
